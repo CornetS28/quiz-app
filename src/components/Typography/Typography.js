7dfb416b847f646d7d1133fa4fc27158357debc1
Samuel Cornet <corsam28@gmail.com>
2021-06-10 11:34:41 -0400

    Create a reusable component for typography texts

---
+++ src/components/Typography/Typography.js
@@ -0,0 +1,20 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+// MUI stuff
+import Typography from "@material-ui/core/Typography";
+
+const TypographyText = ({ size, textClass, text }) => {
+  return (
+    <Typography variant={size} gutterBottom className={textClass}>
+      {text}
+    </Typography>
+  );
+};
+TypographyText.propTypes = {
+  textClass: PropTypes.string.isRequired,
+  size: PropTypes.string.isRequired,
+  text: PropTypes.string.isRequired,
+};
+
+export default TypographyText;
